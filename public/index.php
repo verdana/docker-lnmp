e519bbc2ce32ff6f3c971fc81d6f95725565c68a
Verdana <verdana.cn@gmail.com>
2018-08-24 01:40:47 +0800

    测试 MySQL 连接

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
-phpinfo();
+// Test MySQL connection
+$dsn = 'mysql:host=172.100.0.5;port=3306';
+$user = 'root';
+$password = 'mysql';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $version = $dbh->query('SELECT version()')->fetchColumn();
+    echo 'MySQL ' . $version . ' connected';
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
 
