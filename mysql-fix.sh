dea0b482b2d6049469d9233178683ab058b52943
Verdana <verdana.cn@gmail.com>
2018-08-24 20:57:15 +0800

    命令行中加入 mysql 密码

--- mysql-fix.sh
+++ mysql-fix.sh
@@ -7,7 +7,7 @@ echo "Change the permissions of /var/run/mysqld to 755"
 docker exec -i $mysql_container_id chmod -Rf 755 /var/run/mysqld
 
 echo "Change localhost to 127.0.0.1"
-docker exec -i $mysql_container_id mysql --user=root <<EOF
+docker exec -i $mysql_container_id mysql --user=root --password=mysql <<EOF
     UPDATE mysql.user SET Host='127.0.0.1' WHERE Host='localhost';
     UPDATE mysql.tables_priv SET Host='127.0.0.1' WHERE Host='localhost';
     UPDATE mysql.db SET Host='127.0.0.1' WHERE Host='localhost';
