bb66267c8bb7bb31ca66b388e847bd390f94e969
Verdana <verdana.cn@gmail.com>
2018-08-24 20:01:07 +0800

    修正仅对Arch有效

--- hugepage-fix-arch.sh
+++ hugepage-fix-arch.sh
@@ -4,8 +4,10 @@ echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
 
 # only for Archlinux
+if [ `uname -r | grep -ic arch` -gt 0 ]; then
 cat > /etc/tmpfiles.d/local.conf <<EOF
 w /sys/kernel/mm/transparent_hugepage/enabled - - - - never
 w /sys/kernel/mm/transparent_hugepage/defrag - - - - never
 EOF
+fi
 
